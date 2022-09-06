e2e589a9886533be92417cce021dc01965ebe890
shark826 <gs20062@yandex.ru>
2022-09-06 21:56:23 +0300

    firstview

--- smi2022/views.py
+++ smi2022/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from smi2022.models import *
+
+class GurnalSmi(ListView):
+    model = GurnalSmi
+
