406d3bbc9da99b5ccc154f9814d0faf1c70dbb0f
shark826 <gs20062@yandex.ru>
2022-09-08 21:34:17 +0300

    080922

--- smi2022/views.py
+++ smi2022/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
 from smi2022.models import *
+from django.core.paginator import Paginator
 
 class GurnalSmi(ListView):
+    paginate_by = 2
     model = GurnalSmi
+    #smi = GurnalSmi.objects.all()
+    #p = Paginator(smi, 10)
 
 class GurnalSmiSocNet(ListView):
     model = GurnalSocNet
